11dc626ec09b95cbeaa595abb2154dceefdfd64c
Julián Boetto <julib_8724@hotmail.com>
2023-01-31 16:44:16 -0300

    Primeira configuracao

---
+++ movies-service/src/config/db.test.js
@@ -0,0 +1,12 @@
+import { test, expect } from "@jest/globals";
+import database from "./db";
+
+test("Connection Database", async () => {
+    const connection = await database.connect();
+    expect(connection).toBeTruthy();
+});
+
+test("Disconnection Database", async () => {
+    const isDisconnected = await database.disconnect();
+    expect(isDisconnected).toBeTruthy();
+});
--- movies-service/src/jest.config.mjs
+++ movies-service/src/jest.config.mjs
@@ -33,5 +33,5 @@ export default {
   ],
   testPathIgnorePatterns: [
     "/node_modules/"
-  ],
+  ]
 };
