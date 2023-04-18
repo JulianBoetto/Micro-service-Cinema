3361411ed6445d7f5c1760ef50a6777ea1e6dfd9
Julián Boetto <julib_8724@hotmail.com>
2023-04-18 16:32:36 -0300

    Movies pela cidade, movies id e cinemas pela cidade testados

--- cinema-catalog-services/src/api/cinema-catalog-service.test.js
+++ cinema-catalog-services/src/api/cinema-catalog-service.test.js
@@ -7,19 +7,46 @@ import request from "supertest";
 let app = null;
 
 beforeAll(async () => {
-    process.env.PORT = 3004;
-    app = await server.start(cinemaCatalog, repositoryMock);
-})
+  process.env.PORT = 3004;
+  app = await server.start(cinemaCatalog, repositoryMock);
+});
 
 afterAll(async () => {
-    await server.stop();
-})
+  await server.stop();
+});
 
 describe("Cinema Catalog Services 200 OK", () => {
-    test('GET /cities 200 OK', async () => {
-        const response = await request(app).get('/cities');
-        expect(response.status).toEqual(200);
-        expect(Array.isArray(response.body)).toBeTruthy();
-        expect(response.body.length).toBeTruthy();
-    })
-})
+  test("GET /cities 200 OK", async () => {
+    const response = await request(app).get("/cities");
+    expect(response.status).toEqual(200);
+    expect(Array.isArray(response.body)).toBeTruthy();
+    expect(response.body.length).toBeTruthy();
+  });
+
+  test("GET /cities/:cityId/movies 200 OK", async () => {
+    const testCityId = "1";
+    const response = await request(app).get(`/cities/${testCityId}/movies`);
+    expect(response.status).toEqual(200);
+    expect(response.body).toBeTruthy();
+  });
+
+  test("GET /cities/:cityId/movies/:movieId 200 OK", async () => {
+    const testCityId = "1";
+    const testMovieId = "1";
+    const response = await request(app).get(
+      `/cities/${testCityId}/movies/${testMovieId}`
+    );
+    expect(response.status).toEqual(200);
+    expect(response.body).toBeTruthy();
+  });
+
+  test("GET /cities/:cityId/cinemas 200 OK", async () => {
+    const testCityId = "1";
+    const response = await request(app)
+      .get(`/cities/${testCityId}/cinemas`)
+    expect(response.status).toEqual(200);
+    expect(response.body).toBeTruthy();
+  });
+});
+
+describe("Cinema Catalog Services 401/404 ERROR", () => {});
