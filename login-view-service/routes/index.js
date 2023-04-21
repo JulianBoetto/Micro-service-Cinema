36a16cc5b617ad0340d449ea0457aaf430169bec
Julián Boetto <julib_8724@hotmail.com>
2023-04-21 19:34:47 -0300

    Renderizando tabela

--- login-view-service/routes/index.js
+++ login-view-service/routes/index.js
@@ -1,6 +1,22 @@
 import { Router } from "express";
 import authController from "../controllers/authController.js";
 const router = Router();
+const movies = [
+  {
+    id: 1,
+    col1: "random",
+    col2: "data",
+    col3: "placeholder",
+    col4: "text",
+  },
+  {
+    id: 2,
+    col1: "placeholder",
+    col2: "irrelevant",
+    col3: "visual",
+    col4: "layout",
+  },
+];
 
 router.get("/:pagina?", authController.validateToken, (req, res, next) => {
   res.render("index", {
@@ -9,6 +25,7 @@ router.get("/:pagina?", authController.validateToken, (req, res, next) => {
     qtd: 2,
     qtdPaginas: 4,
     profile: "req.user.profile",
+    data: movies
   });
 });
 
