e784c429c4028ec1cc0ba7eed2a05a5cf57a2747
Julián Boetto <julib_8724@hotmail.com>
2023-04-22 16:43:45 -0300

    Showing the id in hte query params

--- api-gateway/src/server/index.test.js
+++
@@ -1,8 +0,0 @@
-import optionsProxy from "../utils/optionsProxy.js";
-
-it("Should return the original URL", () => {
-  const req = {
-    originalUrl: "https://my-website.com/my-page",
-  };
-  expect(optionsProxy.proxyReqPathResolver(req)).toEqual(req.originalUrl);
-});
--- login-view-service/controllers/moviesController.js
+++ login-view-service/controllers/moviesController.js
@@ -2,17 +2,25 @@ import moviesRepository from "../repository/movies.js";
 import cinemaRepository from "../repository/cinema.js";
 
 async function getMovies(req, res, next) {
+  let filterCityBtn = { _id: "all", city: "All" };
   const token = req.cookies.token;
+  const { city } = req.query;
+
   const response = await moviesRepository.getMovies(token);
   if (!response || response.error) return res.redirect("/logout");
 
   const cities = await cinemaRepository.getCities(token);
-  console.log(cities)
+  if (city !== "all")
+    filterCityBtn = cities.find((el) => {
+      return el._id === city;
+    });
+
   if (!cities || cities.error) return res.redirect("/logout");
 
   res.render("index", {
     movies: response,
     cities: cities,
+    filterCityBtn,
   });
 }
 
